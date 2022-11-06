f8e34d20f646b5a26c4993fe7645c4eb24048a51
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-11-05 20:33:22 -0700

    feat: add client and first method
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- rust-sdk/src/client.rs
+++ rust-sdk/src/client.rs
@@ -1,60 +1,103 @@
-use reqwest;
+use error_stack::{IntoReport, Report, ResultExt};
+use graphql_client::{GraphQLQuery, Response};
+use std::error::Error;
+use std::fmt;
+use std::fmt::{Debug, Display};
 
+#[derive(Debug)]
 pub enum QuickNodeError {
-    DeserializationError(String),
-    RequestError(String),
-    GraphQLError(String),
+    Deserialization,
+    Request,
+    GraphQL(Vec<graphql_client::Error>),
 }
 
 impl Display for QuickNodeError {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Quicknode error")
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("QuickNode error")
     }
 }
 
+impl Error for QuickNodeError {}
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/schema.gql",
     query_path = "src/queries/GetCollectionDetailsQuery.gql",
-    response_derives = "Debug",
+    response_derives = "Debug"
 )]
-pub struct CollectionDetailsQuery;
+pub struct CollectionDetails;
 
 pub struct QuickNodeSDK {
+    base_url: String,
     icy_api_key: String,
     client: reqwest::Client,
 }
 
 impl QuickNodeSDK {
-    pub fn new(icy_api_key: Option<String>) -> Self {
-        if icy_api_key.is_none() {
-            println!("NO API_KEY");
-        }
-        
+    pub fn new(icy_api_key: &str) -> Self {
         Self {
-            icy_api_key: icy_api_key.unwrap_or(String::from("")),
+            base_url: String::from("https://graphql.icy.tools/graphql"),
+            icy_api_key: icy_api_key.to_string(),
             client: reqwest::Client::new(),
         }
     }
 
-    pub fn set_icy_api_key(&mut self, key: String) -> &mut self {
+    pub fn set_icy_api_key(&mut self, key: String) {
         self.icy_api_key = key;
-        self
     }
 
-    pub async fn get_collection_details(address: &str) -> () {
-        let vars = collection_details_query::Variables {
-            address,
+    pub async fn get_collection_details(
+        self,
+        address: &str,
+    ) -> Result<collection_details::ResponseData, Report<QuickNodeError>> {
+        let vars = collection_details::Variables {
+            address: address.to_string(),
         };
 
-        let request_body = CollectionDetailsQuery::build_query(vars);
+        let request_body = CollectionDetails::build_query(vars);
 
-        let res = client.post("https://graphql.icy.tools/graphql")
+        let res = self
+            .client
+            .post(self.base_url)
             .json(&request_body)
             .send()
-            .await?;
-        
-        let response_body: Response<example_query::ResponseData> = res.json().await?;
+            .await
+            .into_report()
+            .attach_printable("failed to perform POST".to_string())
+            .change_context(QuickNodeError::Request)?;
+
+        let response_body: Response<collection_details::ResponseData> = res
+            .json()
+            .await
+            .into_report()
+            .attach_printable("failed to deserialize response".to_string())
+            .change_context(QuickNodeError::Deserialization)?;
+
+        if let Some(errors) = response_body.errors {
+            return Err(Report::new(QuickNodeError::GraphQL(errors)));
+        }
+
+        match response_body.data {
+            Some(data) => Ok(data),
+            None => Err(Report::new(QuickNodeError::Request)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn it_works() {
+        let sdk = QuickNodeSDK::new(None);
+        let res = sdk
+            .get_collection_details("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D")
+            .await;
+
+        match res {
+            Ok(collection_details) => println!("{:?}", collection_details),
+            Err(err) => println!("{:?}", err),
+        }
     }
 }
