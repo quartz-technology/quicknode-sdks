5f1e941842e7aa16c8b4c75c00d47280b516dd16
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-11-05 20:24:39 -0700

    chore: update go module to follow repo name
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- go/client/endpoint.go
+++
@@ -1,6 +0,0 @@
-package client
-
-// Endpoint is the HTTP endpoint of GraphQL Icy Tools.
-// You can also access the playground at
-// https://graphql.icy.tools/playground
-const Endpoint = "https://graphql.icy.tools/graphql"
--- go/spec/log.go
+++
@@ -1,9 +0,0 @@
-package spec
-
-type LogType string
-
-const (
-	TRANSFER LogType = "TRANSFER"
-	ORDER    LogType = "ORDER"
-	MINT     LogType = "MINT"
-)
